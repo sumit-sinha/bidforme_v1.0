0e9045db8fd6c662281adff635cf8365c5b01791
Simone Campora <simone.campora@gmail.com>
2014-07-21 11:35:32 +0200

    re-adding parallax animation for icons
    Signed-off-by: Simone Campora <simone.campora@gmail.com>

--- war/resources/scripts/parallax/initialize.js
+++ war/resources/scripts/parallax/initialize.js
@@ -116,6 +116,25 @@ $('body').scrollspy({
 
  });
 
+$(".motto a").on('click', function(e) {
+    
+    target = this.hash;
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(this.hash).offset().top 
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = target;
+     });
+
+});
+
+
  $(document).ready(function () {
      // cache the window object
      $window = $(window);
